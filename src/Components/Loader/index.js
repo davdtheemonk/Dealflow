1213dffe96fb08877a05ba64287965c21e622a15
ProbablyX <83953506+davdtheemonk@users.noreply.github.com>
2023-06-23 17:24:37 +0300

    cleared errors

--- src/Components/Loader/index.js
+++ src/Components/Loader/index.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Loader() {
   return (
     <div className="flex w-full min-h-[100vh]  items-center justify-center">
-      <img className="w-10" src="/loader.gif" />
+      <img alt="loader" className="w-10" src="/loader.gif" />
     </div>
   );
 }
